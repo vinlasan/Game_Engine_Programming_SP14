846b8682a263c60433d548d47288a69d6c1d5bf9
Vincent <vinlasan@hotmail.com>
2014-02-05 21:06:01 -0600

    misc update

---
+++ GEP_Adventure_Program/Stage1.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Stage1
+{
+private: 
+	vector<string> scenes;
+public:
+	Stage1(void);
+	~Stage1(void);
+	void DisplayScenes(void);
+};
